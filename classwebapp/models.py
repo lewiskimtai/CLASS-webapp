01c9475ee4f3dc0ee732a6c223e8118f614fd1c2
[Lewis Kimtai] <[lewis.kimtai@gmail.com]>
2019-06-07 18:02:15 +0300

    relationship

--- classwebapp/models.py
+++ classwebapp/models.py
@@ -15,7 +15,7 @@ class Student(db.Model, UserMixin):
     student_email = db.Column(db.String(120), unique=True, nullable=False)
     student_password = db.Column(db.String(60), nullable=False)
     #active = db.Column('is_active', db.Boolean(), nullable=False, server_default=False)
-    role = db.relationship('Role', secondary='UserRole', backref=db.backref('student', lazy='dynamic'))
+    role = db.relationship('Role', backref='student', lazy='dynamic')
 
     def __repr__(self):
         return f"User('{self.student_name}', '{self.student_email}')"
@@ -26,7 +26,7 @@ class Lecturer(db.Model, UserMixin):
     lecturer_email = db.Column(db.String(120), unique=True, nullable=False)
     lecturer_password = db.Column(db.String(60), nullable=False)
     #active = db.Column('is_active', db.Boolean(), nullable=False, server_default=True)
-    role = db.relationship('Role', secondary='UserRole', backref=db.backref('lecturer', lazy='dynamic'))
+    role = db.relationship('Role', backref='lecturer', lazy='dynamic')
     
 
     def __repr__(self):
@@ -36,16 +36,16 @@ class Lecturer(db.Model, UserMixin):
 class Role(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     role = db.Column(db.String(20), unique=False, nullable=False)
-    role = db.relationship('Role', secondary='UserRole', backref=db.backref('role', lazy='dynamic'))
+    role = db.relationship('Role', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return f"User('{self.role_id}')"
 
 class UserRole(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    role_id = db.Column(db.Integer(), db.ForeignKey('role.id', ondelete='CASCADE'))
-    student_id = db.Column(db.Integer(), db.ForeignKey('student.id', ondelete='CASCADE'))
-    lecturer_id = db.Column(db.Integer(), db.ForeignKey('lecturer.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('role.id'))
+    student_id = db.Column(db.Integer(), db.ForeignKey('student.id'))
+    lecturer_id = db.Column(db.Integer(), db.ForeignKey('lecturer.id'))
 
     def __repr__(self):
         return f"User('{self.role_id}', '{self.student_id}', '{self.lecturer_id}')"
